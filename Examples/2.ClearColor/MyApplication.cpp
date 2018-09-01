6b63646a606fb11a5b089471edcb59e4486e6f64
FRED\fedorius <fedoriusv@gmail.com>
2018-09-01 23:07:34 +0300

    Added ClarColor project

--- Examples/2.ClearColor/MyApplication.cpp
+++ Examples/2.ClearColor/MyApplication.cpp
@@ -2,7 +2,9 @@
 #include "Utils/Logger.h"
 
 #include "Renderer/Context.h"
-#include "Renderer/CommandList.h"
+#include "Event/InputEventReceiver.h"
+#include "Platform/Window.h"
+
 
 using namespace v3d;
 using namespace v3d::platform;
@@ -12,6 +14,9 @@ using namespace v3d::event;
 MyApplication::MyApplication(int& argc, char** argv)
     : m_Window(nullptr)
     , m_InputEventHandler(nullptr)
+
+    , m_Context(nullptr)
+    , m_CommandList(nullptr)
 {
     m_Window = Window::createWindow({ 1024, 768 }, {800, 500}, false, new v3d::event::InputEventReceiver());
     ASSERT(m_Window, "windows is nullptr");
@@ -66,7 +71,7 @@ int MyApplication::Execute()
 
         m_Window->getInputEventReceiver()->sendDeferredEvents();
 
-        MyApplication::Running();
+        MyApplication::Running(*m_CommandList);
     }
 
     Exit();
@@ -77,35 +82,23 @@ int MyApplication::Execute()
 
 void MyApplication::Initialize()
 {
-    //TEST
-    LOG_DEBUG("Debug Test");
-    LOG("Test");
-    LOG_INFO("Info Test");
-    LOG_WARNING("Warrnig Test");
-    LOG_ERROR("Error Test");
-    LOG_FATAL("Fatal Test");
+    m_Context = renderer::Context::createContext(m_Window, renderer::Context::RenderType::VulkanRender);
+    ASSERT(m_Context, "context is nullptr");
 
-    char cvar[] = "CHAR";
-    int ivar = 0;
-
-    LOG_DEBUG("Debug Test, %s, %d", cvar, ivar++);
-    LOG("Test, %s, %d", cvar, ivar++);
-    LOG_INFO("Info Test, %s, %d", cvar, ivar++);
-    LOG_WARNING("Warrnig Test, %s, %d", cvar, ivar++);
-    LOG_ERROR("Error Test, %s, %d", cvar, ivar++);
-    LOG_FATAL("Fatal Test, %s, %d", cvar, ivar++);
+    m_CommandList = new renderer::CommandList(m_Context, renderer::CommandList::CommandListType::DelayedCommandList);
 }
 
-bool MyApplication::Running()
+bool MyApplication::Running(renderer::CommandList& commandList)
 {
-    /*
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    m_Window->minimize();
-    m_Window->isActive();
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    m_Window->restore();
-    m_Window->isActive();
-    */
+    //Frame
+    commandList.cmdBeginFrame();
+    commandList.cmdEndFrame();
+
+    commandList.flushCommands();
+
+    commandList.cmdPresentFrame();
+    //Frame
+
     return true;
 }
 
@@ -113,19 +106,12 @@ void MyApplication::Exit()
 {
     m_Window->getInputEventReceiver()->dettach(InputEvent::InputEventType::KeyboardInputEvent);
     m_Window->getInputEventReceiver()->dettach(InputEvent::InputEventType::MouseInputEvent);
-
-    if (m_InputEventHandler)
-    {
-        delete m_InputEventHandler;
-        m_InputEventHandler = nullptr;
-    }
 }
 
 MyApplication::~MyApplication()
 {
-    if (m_Window)
-    {
-        Window::detroyWindow(m_Window);
-        m_Window = nullptr;
-    }
+    delete m_InputEventHandler;
+    m_InputEventHandler = nullptr;
+
+    Window::detroyWindow(m_Window);
 }
---
+++ Examples/2.ClearColor/MyApplication.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Common.h"
+#include "Renderer/CommandList.h"
+
+#include "Platform/Window.h"
+#include "Renderer/Context.h"
+#include "Event/InputEventHandler.h"
+
+class MyApplication
+{
+public:
+
+    MyApplication(int& argc, char** argv);
+    ~MyApplication();
+
+    int Execute();
+
+private:
+
+    void Initialize();
+    bool Running(v3d::renderer::CommandList& cmd);
+    void Exit();
+
+    v3d::platform::Window* m_Window;
+    v3d::event::InputEventHandler* m_InputEventHandler;
+
+    v3d::renderer::Context* m_Context;
+    v3d::renderer::CommandList* m_CommandList;
+};
