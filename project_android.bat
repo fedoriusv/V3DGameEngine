7c2179ef0b1f70e446e6e48fbc5a93d7c4c903ec
Fedir Nekrasov/Core Rendering Part /SRK/Engineer/삼성전자 <f.nekrasov@samsung.com>
2019-05-15 12:42:14 +0300

    fix gradle pack

--- project_android.bat
+++ project_android.bat
@@ -1,6 +1,13 @@
 @echo off
 
 setlocal
+
+set C_ANDROID_TOOLCHAIN=%ANDROID_NDK%\build\cmake\android.toolchain.cmake
+set C_ANDROID_NDK=%ANDROID_NDK%
+set C_ANDROID_ABI=arm64-v8a
+set C_ANDROID_PLATFORM=android-26
+set C_BUILD_TYPE=Debug
+
 set command=all
 
 if "%ANDROID_NDK%" == "" (
@@ -30,12 +37,22 @@ if "%1" == "--build" (
     goto build
 )
 
+if "%1" == "--pack" (
+    if "%2" == "" (
+        echo Error. Name of pack is empty or not exist
+        goto end
+    )
+    
+    set command=single
+    goto pack
+)
+
 :help
     echo Build Android
     echo --clean          Clean build folder
     echo --prepare        Generate ninja cmake project
     echo --build          Build Android .so file
-    echo --pack           Pack resources
+    echo --pack "name"    Pack resources
     goto end
 
 :all
@@ -53,12 +70,6 @@ if "%1" == "--build" (
     )
     cd Project\Android
 
-    set C_ANDROID_TOOLCHAIN=%ANDROID_NDK%\build\cmake\android.toolchain.cmake
-    set C_ANDROID_NDK=%ANDROID_NDK%
-    set C_ANDROID_ABI=arm64-v8a
-    set C_ANDROID_PLATFORM=android-26
-    set C_BUILD_TYPE=Debug
-
     cmake -DCMAKE_TOOLCHAIN_FILE=%C_ANDROID_TOOLCHAIN% -DANDROID_NDK=%C_ANDROID_NDK% -DANDROID_ABI=%C_ANDROID_ABI% -DANDROID_PLATFORM=%C_ANDROID_PLATFORM% -DCOMPILER_CLANG=ON -DTARGET_ANDROID=ON -DCMAKE_MAKE_PROGRAM=Ninja -GNinja -DCMAKE_BUILD_TYPE=%C_BUILD_TYPE% -DCRC32C_BUILD_TESTS=OFF -DCRC32C_BUILD_BENCHMARKS=OFF -DCRC32C_USE_GLOG=OFF -DCRC32C_INSTALL=OFF -DASSIMP_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=OFF ../..
     cd ..\..
     if "%command%" == "single" goto end
@@ -73,22 +84,26 @@ if "%1" == "--build" (
     
     cmake --build .
     cd ..\..
-    if "%command%" == "single" goto end
+rem    if "%command%" == "single" goto end
+    goto end
     
 :pack
     echo Pack
-    if not exist Project\Android (
-        echo Error. Start prepare first
+    if not exist Project\Android\Examples\%2 (
+        echo Error. Start prepare and build first
         goto end
     )
-    cd Project\Android
     
-rem    mkdir "assets"
-rem	   xcopy "..\data\*.*" "assets" /SY
-
-    cd ..\..
+    xcopy Examples\%2\AndroidManifest.xml Project\Android\Examples\%2
+    xcopy Examples\%2\build.gradle Project\Android\Examples\%2
+    
+    mkdir "Project\Android\Examples\%2\libs\%C_ANDROID_ABI%"
+    xcopy Project\Android\Examples\%2\lib%2.so Project\Android\Examples\%2\libs\%C_ANDROID_ABI%
+    
+    cd Config
+    call gradlew.bat -p Project/Android/Examples/%2 build
+    cd ..
     if "%command%" == "single" goto end
-    
     
 :end
 if "%command%" == "all" pause
