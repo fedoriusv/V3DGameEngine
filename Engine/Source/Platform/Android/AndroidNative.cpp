38c507495a08a060306361e05dc19a12a831f386
fedoriusv <fedoriusv@gmail.com>
2020-11-16 02:14:52 +0200

    Add android props

--- Engine/Source/Platform/Android/AndroidNative.cpp
+++ Engine/Source/Platform/Android/AndroidNative.cpp
@@ -2,6 +2,7 @@
 #include "Utils/Logger.h"
 
 #include <android/log.h>
+#include <sys/system_properties.h>
 #include "android_native_app_glue.h"
 
 extern int main(int argc, char* argv[]);
@@ -12,8 +13,26 @@ void android_main(android_app* state)
     LOG_INFO("Android main started");
     g_nativeAndroidApp = state;
 
-    //TODO
-    main(0, nullptr);
+    //https://stackoverflow.com/questions/7183627/retrieve-android-os-build-system-properties-via-purely-native-android-app
+    char tempStr[32];
+    int size = 0;
+    size = __system_property_get("ro.product.model", tempStr);
+    v3d::android::g_androidSystemProperties._productModel.assign(tempStr, size);
+    size = __system_property_get("ro.product.manufacturer", tempStr);
+    v3d::android::g_androidSystemProperties._productManufacturer.assign(tempStr, size);
+    size = __system_property_get("ro.product.device", tempStr);
+    v3d::android::g_androidSystemProperties._productDevice.assign(tempStr, size);
+    size = __system_property_get("ro.build.version.release", tempStr);
+    v3d::android::g_androidSystemProperties._buildVersionRelease.assign(tempStr, size);
+
+    LOG_DEBUG("Android product.model: %s", v3d::android::g_androidSystemProperties._productModel.data());
+    LOG_DEBUG("Android product.manufacturer: %s", v3d::android::g_androidSystemProperties._productManufacturer.data());
+    LOG_DEBUG("Android product.device: %s", v3d::android::g_androidSystemProperties._productDevice.data());
+    LOG_DEBUG("Android build.version.release: %s", v3d::android::g_androidSystemProperties._buildVersionRelease.data());
+
+    int argc = 0;
+    char** argv = nullptr;
+    main(argc, argv);
 }
 
 void android_log_arg(v3d::utils::Logger::LoggerType type, const char* message, va_list ap)
--- Engine/Source/Platform/Android/AndroidNative.h
+++ Engine/Source/Platform/Android/AndroidNative.h
@@ -4,4 +4,21 @@
 
 struct android_app;
 
+namespace v3d
+{
+namespace android
+{
+    struct AndroidSystemProperties
+    {
+        std::string _productModel;
+        std::string _productManufacturer;
+        std::string _productDevice;
+        std::string _buildVersionRelease;
+    };
+
+    inline AndroidSystemProperties g_androidSystemProperties = {};
+
+} //namespace android
+} //namespace v3d
+
 extern "C" void android_main(android_app* state);
