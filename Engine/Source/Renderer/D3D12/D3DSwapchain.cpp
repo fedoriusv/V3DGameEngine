49d12d0b0faa7ec80a125fcc5781c0587c586ae0
fedoriusv <fedoriusv@gmail.com>
2021-12-02 19:34:43 +0200

    [XB] rework swapchain

--- Engine/Source/Renderer/D3D12/D3DSwapchain.cpp
+++ Engine/Source/Renderer/D3D12/D3DSwapchain.cpp
@@ -14,13 +14,11 @@ namespace renderer
 namespace dx3d
 {
 
-D3DSwapchain::D3DSwapchain(IDXGIFactory4* factory, ID3D12Device* device, ID3D12CommandQueue* cmdQueue, D3DDescriptorHeapManager* heapMgr) noexcept
-    : m_factory(factory)
-    , m_device(device)
+D3DSwapchain::D3DSwapchain(ID3D12Device* device, ID3D12CommandQueue* cmdQueue, D3DDescriptorHeapManager* heapMgr) noexcept
+    : m_device(device)
     , m_commandQueue(cmdQueue)
     , m_heapManager(heapMgr)
 
-    , m_swapChain(nullptr)
     , m_descriptorHeap(nullptr)
 
     , m_frameIndex(0U)
@@ -34,10 +32,44 @@ D3DSwapchain::~D3DSwapchain()
     LOG_DEBUG("D3DSwapchain::~D3DSwapchain destructor %llx", this);
 
     ASSERT(!m_descriptorHeap, "not nullptr");
+}
+
+bool D3DSwapchain::resize(const SwapchainConfig& config)
+{
+    ASSERT(false, "not impl");
+    return false;
+}
+
+D3DImage* D3DSwapchain::getSwapchainImage() const
+{
+    ASSERT(m_renderTargets[m_frameIndex], "nullptr");
+    return m_renderTargets[m_frameIndex];
+}
+
+bool D3DSwapchain::vsync() const
+{
+    return m_syncInterval > 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#if defined(PLATFORM_WINDOWS)
+D3DSwapchainWindows::D3DSwapchainWindows(IDXGIFactory4* factory, ID3D12Device* device, ID3D12CommandQueue* cmdQueue, D3DDescriptorHeapManager* heapMgr) noexcept
+    : D3DSwapchain(device, cmdQueue, heapMgr)
+    , m_factory(factory)
+    , m_swapChain(nullptr)
+{
+    LOG_DEBUG("D3DSwapchainWindows::D3DSwapchainWindows constructor %llx", this);
+}
+
+D3DSwapchainWindows::~D3DSwapchainWindows()
+{
+    LOG_DEBUG("D3DSwapchainWindows::~D3DSwapchainWindows destructor %llx", this);
+
     ASSERT(!m_swapChain, "not nullptr");
 }
 
-bool D3DSwapchain::create(const SwapchainConfig& config)
+bool D3DSwapchainWindows::create(const SwapchainConfig& config)
 {
     Format format = Format::Format_R8G8B8A8_UNorm;
     m_syncInterval = config._vsync ? 1 : 0;
@@ -76,7 +108,7 @@ bool D3DSwapchain::create(const SwapchainConfig& config)
         if (FAILED(result))
         {
             LOG_ERROR("D3DSwapchain::create MakeWindowAssociation is failed. Error %s", D3DDebug::stringError(result).c_str());
-            D3DSwapchain::destroy();
+            D3DSwapchainWindows::destroy();
 
             return false;
         }
@@ -92,11 +124,11 @@ bool D3DSwapchain::create(const SwapchainConfig& config)
         for (u32 index = 0; index < config._countSwapchainImages; index++)
         {
             ID3D12Resource* swapchainImage;
-            HRESULT result = m_swapChain->GetBuffer(index, IID_PPV_ARGS(&swapchainImage));
+            HRESULT result = m_swapChain->GetBuffer(index, DX_IID_PPV_ARGS(&swapchainImage));
             if (FAILED(result))
             {
                 LOG_ERROR("D3DSwapchain::create GetBuffer is failed. Error %s", D3DDebug::stringError(result).c_str());
-                D3DSwapchain::destroy();
+                D3DSwapchainWindows::destroy();
 
                 return false;
             }
@@ -110,7 +142,7 @@ bool D3DSwapchain::create(const SwapchainConfig& config)
             if (!image->create(swapchainImage, imageHandle))
             {
                 LOG_ERROR("D3DSwapchain::create swapimage is failed");
-                D3DSwapchain::destroy();
+                D3DSwapchainWindows::destroy();
 
                 return false;
             }
@@ -124,7 +156,7 @@ bool D3DSwapchain::create(const SwapchainConfig& config)
     return true;
 }
 
-void D3DSwapchain::destroy()
+void D3DSwapchainWindows::destroy()
 {
     for (auto& image : m_renderTargets)
     {
@@ -141,7 +173,9 @@ void D3DSwapchain::destroy()
     SAFE_DELETE(m_swapChain);
 }
 
-void D3DSwapchain::present()
+
+
+void D3DSwapchainWindows::present()
 {
     static DXGI_PRESENT_PARAMETERS presentParameters = {};
     presentParameters.DirtyRectsCount = 0;
@@ -157,29 +191,163 @@ void D3DSwapchain::present()
     }
 }
 
-u32 D3DSwapchain::acquireImage()
+u32 D3DSwapchainWindows::acquireImage()
 {
     m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
     return m_frameIndex;
 }
 
-bool D3DSwapchain::resize(const SwapchainConfig& config)
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#elif defined(PLATFORM_XBOX)
+D3DSwapchainXBOX::D3DSwapchainXBOX(IDXGIAdapter* adapter, ID3D12Device* device, ID3D12CommandQueue* cmdQueue, D3DDescriptorHeapManager* heapMgr) noexcept
+    : D3DSwapchain(device, cmdQueue, heapMgr)
+    , m_adapter(adapter)
+    , m_frameToken(D3D12XBOX_FRAME_PIPELINE_TOKEN_NULL)
+    , m_colorSpace(DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P709)
 {
-    ASSERT(false, "not impl");
-    return false;
+    LOG_DEBUG("D3DSwapchainXBOX::D3DSwapchainXBOX constructor %llx", this);
 }
 
-D3DImage* D3DSwapchain::getSwapchainImage() const
+D3DSwapchainXBOX::~D3DSwapchainXBOX()
 {
-    ASSERT(m_renderTargets[m_frameIndex], "nullptr");
-    return m_renderTargets[m_frameIndex];
+    LOG_DEBUG("D3DSwapchainXBOX::~D3DSwapchainXBOX destructor %llx", this);
 }
 
-bool D3DSwapchain::vsync() const
+bool D3DSwapchainXBOX::create(const SwapchainConfig& config)
 {
-    return m_syncInterval > 0;
+    Format format = Format::Format_R8G8B8A8_UNorm;
+    m_syncInterval = 1; //always vsync
+
+    {
+        m_descriptorHeap = m_heapManager->allocateDescriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE_RTV, config._countSwapchainImages, D3D12_DESCRIPTOR_HEAP_FLAG_NONE);
+        ASSERT(m_descriptorHeap, "nullptr");
+
+        CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_descriptorHeap->getCPUHandle());
+        m_renderTargets.reserve(config._countSwapchainImages);
+
+
+        CD3DX12_HEAP_PROPERTIES swapChainHeapProperties(D3D12_HEAP_TYPE_DEFAULT);
+
+        D3D12_RESOURCE_DESC swapChainBufferDesc = CD3DX12_RESOURCE_DESC::Tex2D(D3DImage::convertImageFormatToD3DFormat(format), config._size.width, config._size.height, 1, 1);
+        swapChainBufferDesc.Flags |= D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET;
+
+        D3D12_CLEAR_VALUE swapChainOptimizedClearValue = {};
+        swapChainOptimizedClearValue.Format = D3DImage::convertImageFormatToD3DFormat(format);
+
+        for (u32 index = 0; index < config._countSwapchainImages; index++)
+        {
+            ID3D12Resource* swapchainImage = nullptr;
+            HRESULT result = m_device->CreateCommittedResource(&swapChainHeapProperties, D3D12_HEAP_FLAG_ALLOW_DISPLAY, &swapChainBufferDesc, D3D12_RESOURCE_STATE_PRESENT, &swapChainOptimizedClearValue, DX_IID_PPV_ARGS(&swapchainImage));
+            if (FAILED(result))
+            {
+                LOG_ERROR("D3DSwapchainXBOX::create CreateCommittedResource is failed. Error %s", D3DDebug::stringError(result).c_str());
+                D3DSwapchainXBOX::destroy();
+
+                return false;
+            }
+
+            m_device->CreateRenderTargetView(swapchainImage, nullptr, rtvHandle);
+            rtvHandle.Offset(1, m_descriptorHeap->getIncrement());
+
+            CD3DX12_CPU_DESCRIPTOR_HANDLE imageHandle(m_descriptorHeap->getCPUHandle(), index, m_descriptorHeap->getIncrement());
+            D3DImage* image = new D3DImage(m_device, format, config._size.width, config._size.height, 1U, 1U, TextureUsage::TextureUsage_Attachment | TextureUsage::TextureUsage_Sampled | TextureUsage::TextureUsage_Read, "SwapchainImage_" + std::to_string(index));
+            if (!image->create(swapchainImage, imageHandle))
+            {
+                LOG_ERROR("D3DSwapchainXBOX::create swapimage is failed");
+                D3DSwapchainXBOX::destroy();
+
+                return false;
+            }
+
+            m_renderTargets.push_back(image);
+        }
+    }
+
+    {
+        IDXGIOutput* dxgiOutput;
+        ASSERT(SUCCEEDED(m_adapter->EnumOutputs(0, &dxgiOutput)) && dxgiOutput, "failed");
+
+        u32 timeInterval = m_syncInterval > 1 ? D3D12XBOX_FRAME_INTERVAL_30_HZ : D3D12XBOX_FRAME_INTERVAL_60_HZ;
+        HRESULT result = m_device->SetFrameIntervalX(dxgiOutput, timeInterval, static_cast<UINT>(m_renderTargets.size()) - 1, D3D12XBOX_FRAME_INTERVAL_FLAG_NONE);
+        if (FAILED(result))
+        {
+            LOG_ERROR("D3DSwapchainXBOX::create SetFrameIntervalX is failed. Error %s", D3DDebug::stringError(result).c_str());
+            D3DSwapchainXBOX::destroy();
+
+            return false;
+        }
+    }
+
+    {
+        HRESULT result = m_device->ScheduleFrameEventX(D3D12XBOX_FRAME_EVENT_ORIGIN, 0U, nullptr, D3D12XBOX_SCHEDULE_FRAME_EVENT_FLAG_NONE);
+        if (FAILED(result))
+        {
+            LOG_ERROR("D3DSwapchainXBOX::create ScheduleFrameEventX is failed. Error %s", D3DDebug::stringError(result).c_str());
+            D3DSwapchainXBOX::destroy();
+
+            return false;
+        }
+    }
+
+    m_frameIndex = 0;
+
+    return true;
 }
 
+void D3DSwapchainXBOX::destroy()
+{
+    for (auto& image : m_renderTargets)
+    {
+        image->destroy();
+        delete image;
+    }
+    m_renderTargets.clear();
+    m_heapManager->deallocDescriptorHeap(m_descriptorHeap);
+    m_descriptorHeap = nullptr;
+}
+
+void D3DSwapchainXBOX::present()
+{
+    std::vector<ID3D12Resource*> dxResources;
+    dxResources.push_back(m_renderTargets[m_frameIndex]->getResource());
+
+    D3D12XBOX_PRESENT_PLANE_PARAMETERS planeParameters = {};
+    planeParameters.Token = m_frameToken;
+    planeParameters.ResourceCount = static_cast<UINT>(dxResources.size());
+    planeParameters.ppResources = dxResources.data();
+    planeParameters.pSrcViewRects = nullptr;
+    planeParameters.pDestPlacementBase = nullptr;
+    planeParameters.ColorSpace = m_colorSpace;
+    planeParameters.ScaleFilter = D3D12XBOX_PRESENT_PLANE_SCALE_FILTER_DEFAULT;
+    planeParameters.ExtendedDescCount = 0;
+    planeParameters.pExtendedDescs = nullptr;
+    planeParameters.Flags = D3D12XBOX_PRESENT_PLANE_FLAG_NONE;
+
+    HRESULT result = m_commandQueue->PresentX(1, &planeParameters, nullptr);
+    if (FAILED(result))
+    {
+        LOG_ERROR("D3DSwapchain::present PresentX is failed %s", D3DDebug::stringError(result).c_str());
+        ASSERT(false, "present is failed");
+    }
+}
+
+u32 D3DSwapchainXBOX::acquireImage()
+{
+    m_frameToken = D3D12XBOX_FRAME_PIPELINE_TOKEN_NULL;
+    HRESULT result = m_device->WaitFrameEventX(D3D12XBOX_FRAME_EVENT_ORIGIN, INFINITE, nullptr, D3D12XBOX_WAIT_FRAME_EVENT_FLAG_NONE, &m_frameToken);
+    if (FAILED(result))
+    {
+        LOG_ERROR("D3DSwapchainXBOX::acquireImage WaitFrameEventX is failed. Error %s", D3DDebug::stringError(result).c_str());
+        D3DSwapchainXBOX::destroy();
+
+        return false;
+    }
+
+    m_frameIndex = (m_frameIndex + 1) % m_renderTargets.size();
+    return m_frameIndex;
+}
+#endif //PLATFORM
 } //namespace dx3d
 } //namespace renderer
 } //namespace v3d
--- Engine/Source/Renderer/D3D12/D3DSwapchain.h
+++ Engine/Source/Renderer/D3D12/D3DSwapchain.h
@@ -21,9 +21,9 @@ namespace dx3d
     /////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /**
-    * D3DSwapchain final class. DirectX Render side
+    * D3DSwapchain class. DirectX Render side
     */
-    class D3DSwapchain final
+    class D3DSwapchain
     {
     public:
 
@@ -36,14 +36,14 @@ namespace dx3d
             bool                _fullscreen           = false;
         };
 
-        explicit D3DSwapchain(IDXGIFactory4* factory, ID3D12Device* device, ID3D12CommandQueue* cmdQueue, D3DDescriptorHeapManager* heapMgr) noexcept;
-        ~D3DSwapchain();
+        explicit D3DSwapchain(ID3D12Device* device, ID3D12CommandQueue* cmdQueue, D3DDescriptorHeapManager* heapManager) noexcept;
+        virtual ~D3DSwapchain();
 
-        bool create(const SwapchainConfig& config);
-        void destroy();
+        virtual bool create(const SwapchainConfig& config) = 0;
+        virtual void destroy() = 0;
 
-        void present();
-        u32  acquireImage();
+        virtual void present() = 0;
+        virtual u32 acquireImage() = 0;
 
         bool resize(const SwapchainConfig& config);
 
@@ -51,14 +51,12 @@ namespace dx3d
 
         bool vsync() const;
 
-    private:
+    protected:
 
-        IDXGIFactory4* const m_factory;
         ID3D12Device* const m_device;
         ID3D12CommandQueue* m_commandQueue;
         D3DDescriptorHeapManager* const m_heapManager;
 
-        IDXGISwapChain3* m_swapChain;
         D3DDescriptorHeap* m_descriptorHeap;
 
         std::vector<D3DImage*> m_renderTargets;
@@ -69,6 +67,56 @@ namespace dx3d
         friend class D3DGraphicContext;
 
     };
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#if defined(PLATFORM_WINDOWS)
+    /**
+    * D3DSwapchain Windows platform final class. DirectX Render side
+    */
+    class D3DSwapchainWindows final : public D3DSwapchain
+    {
+    public:
+
+        explicit D3DSwapchainWindows(IDXGIFactory4* factory, ID3D12Device* device, ID3D12CommandQueue* cmdQueue, D3DDescriptorHeapManager* heapManager) noexcept;
+        ~D3DSwapchainWindows();
+
+        bool create(const SwapchainConfig& config) override;
+        void destroy() override;
+
+        void present() override;
+        u32 acquireImage() override;
+
+    private:
+
+        IDXGIFactory4* const m_factory;
+        IDXGISwapChain3* m_swapChain;
+    };
+#elif defined(PLATFORM_XBOX)
+    /**
+    * D3DSwapchain XBOX platform final class. DirectX Render side
+    */
+    class D3DSwapchainXBOX final : public D3DSwapchain
+    {
+    public:
+
+        explicit D3DSwapchainXBOX(IDXGIAdapter* adapter, ID3D12Device* device, ID3D12CommandQueue* cmdQueue, D3DDescriptorHeapManager* heapManager) noexcept;
+        ~D3DSwapchainXBOX();
+
+        bool create(const SwapchainConfig& config) override;
+        void destroy() override;
+
+        void present() override;
+        u32 acquireImage() override;
+
+    private:
+
+        IDXGIAdapter* m_adapter;
+        D3D12XBOX_FRAME_PIPELINE_TOKEN m_frameToken;
+
+        DXGI_COLOR_SPACE_TYPE m_colorSpace;
+    };
+#endif //PLATFORM
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////
 
