9c7f26337168a661209e5530fadba39918721c1f
fedoriusv <fedoriusv@gmail.com>
2025-05-31 12:04:36 +0300

    add StringID class

---
+++ Engine/Source/Utils/StringID.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Common.h"
+
+namespace v3d
+{
+namespace utils
+{
+    using StringID = std::string;
+
+    //class StringID
+    //{
+    //public:
+
+    //    StringID();
+    //    ~StringID();
+
+    //private:
+
+
+    //};
+
+} //namespace utils
+} //namespace v3d
