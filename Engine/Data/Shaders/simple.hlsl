a8748027749f5e1155339ff9d8f1f08cc962713b
fedoriusv <fedoriusv@gmail.com>
2025-07-27 20:39:18 +0300

    add unlit_selectable_ps

--- Engine/Data/Shaders/simple.hlsl
+++ Engine/Data/Shaders/simple.hlsl
@@ -4,8 +4,11 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////
 
-[[vk::binding(0, 0)]] ConstantBuffer<Viewport> CB_Viewport : register(b0, space0);
-[[vk::binding(1, 1)]] ConstantBuffer<ModelBuffer> CB_Model : register(b1, space1);
+[[vk::binding(0, 0)]] ConstantBuffer<Viewport> CB_Viewport  : register(b0, space0);
+
+[[vk::binding(1, 1)]] ConstantBuffer<ModelBuffer> CB_Model  : register(b1, space1);
+[[vk::binding(2, 1)]] SamplerState samplerState             : register(s0, space1);
+[[vk::binding(3, 1)]] Texture2D textureBaseColor            : register(t0, space1);
 
 ///////////////////////////////////////////////////////////////////////////////////////
 
@@ -39,6 +42,26 @@ VS_SIMPLE_OUTPUT billboard_vs(uint VertexID : SV_VERTEXID)
 float4 unlit_ps(PS_SIMPLE_INPUT Input) : SV_TARGET0
 {
     return float4(CB_Model.tint.rgb, 1.0);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+struct PS_SIMPLE_SELECTABLE_OUTPUT
+{
+    [[vk::location(0)]] float4 Color    : SV_TARGET0;
+    [[vk::location(1)]] float4 Material : SV_TARGET1;
+};
+
+PS_SIMPLE_SELECTABLE_OUTPUT unlit_selectable_ps(PS_SIMPLE_INPUT Input)
+{
+    float3 color = textureBaseColor.Sample(samplerState, Input.UV).rgb;
+    
+    PS_SIMPLE_SELECTABLE_OUTPUT Output;
+    
+    Output.Color = float4(color * CB_Model.tint.rgb, 1.0);
+    Output.Material = float4(0.0, 0.0, CB_Model.objectID, 0.0);
+
+    return Output;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////
